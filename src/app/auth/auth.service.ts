95b41b931e389a82e7ef1596444c68463fae24a9
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-28 14:27:52 +0100

    section 20

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
@@ -28,25 +28,8 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(errorRes => {
-      /*
-https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
-Common error codes
-
-EMAIL_EXISTS: The email address is already in use by another account.
-OPERATION_NOT_ALLOWED: Password sign-in is disabled for this project.
-TOO_MANY_ATTEMPTS_TRY_LATER: We have blocked all requests from this device due to unusual activity. Try again later.
- */
-      let errorMessage = 'An unknown error occured!';
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email exists already';
-      }
-      return throwError(errorMessage);
-    }));
+    )
+      .pipe(catchError( this.handleError));
   }
 
   login(email: string, password: string) {
@@ -56,5 +39,32 @@ TOO_MANY_ATTEMPTS_TRY_LATER: We have blocked all requests from this device due t
         password: password,
         returnSecureToken: true
       });
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    /*
+https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+Common error codes
+
+EMAIL_EXISTS: The email address is already in use by another account.
+OPERATION_NOT_ALLOWED: Password sign-in is disabled for this project.
+TOO_MANY_ATTEMPTS_TRY_LATER: We have blocked all requests from this device due to unusual activity. Try again later.
+*/
+    let errorMessage = 'An unknown error occured!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email was not found';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
