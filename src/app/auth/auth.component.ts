d74b3dce0e70ba8e017f4239c5bba819049abc74
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-28 14:13:03 +0100

    section 20

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from './auth.service';
+import {AuthResponseData, AuthService} from './auth.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -28,19 +29,26 @@ export class AuthComponent {
 
     this.isLoading = true;
 
-    if(this.isLoginMode) {
-      // ...
+    let authObs: Observable<AuthResponseData>;
+
+    if (this.isLoginMode) {
+     authObs =  this.authService.signup(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(resData => {
-          console.log(resData);
-          this.isLoading = false;
-        }, errorMessage => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
       form.reset();
     }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
   }
+
+
 }
