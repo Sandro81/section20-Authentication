76248d97a9f979fc955902c10a5d579184340d1d
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-28 12:25:17 +0100

    section 20

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,23 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  resfreshTocken: string;
+  expiresIn: string;
+  localId: string;
+}
+
 @Injectable({providedIn: 'root'})
 export class AuthService {
   constructor(private http: HttpClient) {
   }
 
   signup(email: string, password: string) {
-    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCsTGkCaNQTVXIDu54N7FrRT84tog-vcds',
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCsTGkCaNQTVXIDu54N7FrRT84tog-vcds',
       {
         email: email,
         password: password,
