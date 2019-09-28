bb2893ed4595e65a4f3a33254d725094910cb9d6
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-28 09:43:09 +0100

    section 20

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'section20-Authentication'`, () => {
+  it(`should have as title 'Section16-CourseProject-Forms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('section20-Authentication');
+    expect(app.title).toEqual('Section16-CourseProject-Forms');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to section20-Authentication!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Section16-CourseProject-Forms!');
   });
 });
